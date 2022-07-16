80afb60d442e2116a58f90f55c4a633a950ac9de
ClaudioC <kanobuho@gmail.com>
2022-07-16 17:25:53 -0500

    fix selection of suggestion

--- test/GooglePageTest.py
+++ test/GooglePageTest.py
@@ -2,9 +2,8 @@ import pytest
 
 from webtester.GooglePages import GoogleMainPage 
 
-def test_GoogleWorkflow(driver):
-    keyword = "passion fitness"
-    index = 3
+#@pytest.mark.parametrize("keyword,index", [("passion fitness", 3), ("robot", 6), ("lizzard", 8)])
+def test_GoogleWorkflow(driver, keyword="robot", index=3):
 
     google_main_page = GoogleMainPage(driver)
     google_main_page.go()
@@ -12,8 +11,12 @@ def test_GoogleWorkflow(driver):
 
     google_main_page.getSearchWidget().enter(keyword)
     suggestions = google_main_page.getSearchSuggestions().text()
-    for ln in suggestions:
-        print(ln)
+    print("Suggestions found")
+    for idx, ln in enumerate(suggestions, start=1):
+        if idx == index:
+            print(f"\t- '{ln}'", "*")
+        else:
+            print(f"\t- '{ln}'")
 
     assert len(suggestions) > 2, "No suggestions found"
     
