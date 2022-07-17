16d0af6739d550469166bfc5220327f6a0aa0d16
ClaudioC <kanobuho@gmail.com>
2022-07-16 19:33:52 -0500

    add more driver config details

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,12 @@
 import os
 import pytest
+import tempfile
+import shutil
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
+
 
 HEADLESS = os.environ.get("HEADLESS") == "true"
 
@@ -13,6 +16,11 @@ def driver():
     service = Service(executable_path=driver_location, log_path="./chromedriver.log")
     options = Options()
     options.add_argument("start-maximized")
+    options.add_argument("incognito")
+    options.add_argument("disable-extensions")
+    options.add_argument("disable-popup-blocking") # Pop up blocking
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
 
     # References:
     # https://peter.sh/experiments/chromium-command-line-switches/
@@ -23,13 +31,19 @@ def driver():
             'enable-automation', # Disable banner warning chrome is running with automation
         ],
     )
-    
+
+    # Change default download directory
+    tempdir = tempfile.mkdtemp(prefix="webtester-")
+    prefs = {"download.default_directory" : tempdir}
+    options.add_experimental_option("prefs", prefs)
+
     if HEADLESS:
-        options.add_argument("--headless")
+        options.add_argument("headless")
 
     driver = webdriver.Chrome(options=options, service=service)
 
     yield driver
     
     # time.sleep(2)
+    shutil.rmtree(tempdir)
     driver.quit()
